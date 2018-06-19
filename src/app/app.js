65ec47f5a9b8355322816360788c92940129cd69
WilliamShi1 <FeifanShi@Feifans-MacBook-Pro.local>
2018-06-19 00:53:55 -0400

    First commit

--- src/app/app.js
+++ src/app/app.js
@@ -9,6 +9,59 @@ const MODULE_NAME = 'app';
 angular.module(MODULE_NAME, ['ngRoute'])
   .controller('AppCtrl', function ($scope, $route) {
     // Home page controller
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Sunday","Sunday", "Monday", "Monday","Tuesday", "Tuesday","Wednesday", "Wednesday","Thursday", "Thursday","Friday", "Friday","Saturday","Saturday"],
+        datasets: [{
+            label: '# of Steps, red color for current week and blue color for last week',
+            data: [7700, 4000, 5000, 4500, 11000, 7800, 8700, 9800, 17500, 12000, 6000, 5000, 12000, 11000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                  'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 0.5
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
   })
   .controller('LeaderboardCtrl', function ($scope, $http) {
     // Leaderboard controller
@@ -28,6 +81,21 @@ angular.module(MODULE_NAME, ['ngRoute'])
       return Math.floor(min + Math.random() * (max + 1 - min))
     }
   })
+
+  .controller('FriendsCtrl', function ($scope, $http) {
+    // Friends controller
+    $http({
+      method: 'GET',
+      url: 'https://randomuser.me/api/?results=4'
+    }).then(function successCallback(response) {
+      $scope.friends = response.data.results.map(friend => {
+        return friend;
+      });
+    }, function errorCallback(response) {
+      console.log('api call error');
+    });
+  })
+  
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
@@ -37,6 +105,10 @@ angular.module(MODULE_NAME, ['ngRoute'])
       .when('/leaderboard', {
         template: require('./leaderboard.html'),
         controller: 'LeaderboardCtrl'
+      })
+      .when('/friends', {
+        template: require('./friends.html'),
+        controller: 'FriendsCtrl'
       })
     $locationProvider.hashPrefix('');
   });
